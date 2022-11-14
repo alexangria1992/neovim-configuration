9b07b2b3b38769d48a083f08299b8611fc97df7a
alexangria1992 <alex040892@gmail.com>
2022-11-14 23:50:59 +1300

    Snippets: luasnip

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -13,6 +13,8 @@ packer.startup(function (use)
      'svrana/neosolarized.nvim', 
        requires = {'tjdevries/colorbuddy.nvim'}
      }
+     use 'kyazdani42/nvim-web-devicons'
+     use 'L3MON4D3/LuaSnip' -- Snippet
      use 'hoob3rt/lualine.nvim' --Statusline
      use 'onsails/lspkind-nvim' -- vscode-like pictograms
      use 'hrsh7th/cmp-buffer' --nvim-cmp source for buffer words
